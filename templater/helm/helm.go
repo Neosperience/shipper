e952ea50d6209ee3f7d702f3a6647af21b53fcea
Alessandro Gatti <alessandro.gatti@mikamai.com>
2022-04-27 13:14:17 +0200

    Allow multiple image/tags in a single execution
    Signed-off-by: Alessandro Gatti <alessandro.gatti@mikamai.com>

--- templater/helm/helm.go
+++ templater/helm/helm.go
@@ -9,8 +9,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type HelmProviderOptions struct {
-	Ref        string
+type HelmUpdate struct {
 	ValuesFile string
 	Image      string
 	ImagePath  string
@@ -18,34 +17,50 @@ type HelmProviderOptions struct {
 	TagPath    string
 }
 
+type HelmProviderOptions struct {
+	Ref     string
+	Updates []HelmUpdate
+}
+
 func UpdateHelmChart(repository targets.Repository, options HelmProviderOptions) (targets.FileList, error) {
-	file, err := repository.Get(options.ValuesFile, options.Ref)
-	if err != nil {
-		return nil, fmt.Errorf("could not retrieve values.yaml from repository: %w", err)
-	}
+	original := make(map[string][]byte)
+	files := make(map[string]map[string]interface{})
+	for _, update := range options.Updates {
+		if _, ok := files[update.ValuesFile]; !ok {
+			file, err := repository.Get(update.ValuesFile, options.Ref)
+			if err != nil {
+				return nil, fmt.Errorf("could not retrieve %s from repository: %w", update.ValuesFile, err)
+			}
 
-	values := make(map[string]interface{})
-	if err := yaml.Unmarshal(file, values); err != nil {
-		return nil, fmt.Errorf("could not parse values.yaml: %w", err)
-	}
-	if err := patch.SetPath(values, options.ImagePath, options.Image); err != nil {
-		return nil, fmt.Errorf("could not patch image for values.yaml: %w", err)
-	}
-	if err := patch.SetPath(values, options.TagPath, options.Tag); err != nil {
-		return nil, fmt.Errorf("could not patch image for values.yaml: %w", err)
-	}
+			original[update.ValuesFile] = file
+			files[update.ValuesFile] = make(map[string]interface{})
+			if err := yaml.Unmarshal(file, files[update.ValuesFile]); err != nil {
+				return nil, fmt.Errorf("could not parse YAML file %s: %w", update.ValuesFile, err)
+			}
+		}
 
-	byt, err := yaml.Marshal(values)
-	if err != nil {
-		return nil, fmt.Errorf("could not serialize modified file to YAML: %w", err)
-	}
-
-	if bytes.Equal(byt, file) {
-		return targets.FileList{}, nil
+		if err := patch.SetPath(files[update.ValuesFile], update.ImagePath, update.Image); err != nil {
+			return nil, fmt.Errorf("could not patch image for %s: %w", update.ValuesFile, err)
+		}
+		if err := patch.SetPath(files[update.ValuesFile], update.TagPath, update.Tag); err != nil {
+			return nil, fmt.Errorf("could not patch image for %s: %w", update.ValuesFile, err)
+		}
 	}
 
 	diff := make(targets.FileList)
-	diff[options.ValuesFile] = byt
+	for file, content := range files {
+		byt, err := yaml.Marshal(content)
+		if err != nil {
+			return nil, fmt.Errorf("could not serialize modified file to YAML: %w", err)
+		}
+
+		// Skip if there are no changes
+		if bytes.Equal(original[file], byt) {
+			continue
+		}
+
+		diff[file] = byt
+	}
 
 	return diff, nil
 }
