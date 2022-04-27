93fe17ae9ac68e94940a0405f41b51b6f3db28ad
Alessandro Gatti <alessandro.gatti@mikamai.com>
2022-04-27 14:26:24 +0200

    Pluralise env vars, replace interface{} with any
    Signed-off-by: Alessandro Gatti <alessandro.gatti@mikamai.com>

--- patch/path_test.go
+++ patch/path_test.go
@@ -26,14 +26,14 @@ func TestSetPathExisting(t *testing.T) {
 		t.Fatalf("YAML encoding failed: %s", err.Error())
 	}
 
-	asMap := make(map[string]interface{})
+	asMap := make(map[string]any)
 	err = yaml.Unmarshal(byt, asMap)
 	if err != nil {
 		t.Fatalf("YAML decoding failed: %s", err.Error())
 	}
 
 	// Assert that decoding went fine
-	if asMap["nested"].(map[string]interface{})["value"] != "tochange" {
+	if asMap["nested"].(map[string]any)["value"] != "tochange" {
 		t.Fatal("Expected .Nested.Value value is different")
 	}
 
@@ -44,7 +44,7 @@ func TestSetPathExisting(t *testing.T) {
 	}
 
 	// Check that value was changed
-	if asMap["nested"].(map[string]interface{})["value"] != "changed" {
+	if asMap["nested"].(map[string]any)["value"] != "changed" {
 		t.Fatal("Expected .Nested.Value value is different")
 	}
 
@@ -55,13 +55,13 @@ func TestSetPathExisting(t *testing.T) {
 	}
 
 	// Check that value was changed
-	if asMap["nested"].(map[string]interface{})["other-value"] != "new-value" {
+	if asMap["nested"].(map[string]any)["other-value"] != "new-value" {
 		t.Fatal("New value not found or different")
 	}
 }
 
 func TestSetPathEmpty(t *testing.T) {
-	asMap := make(map[string]interface{})
+	asMap := make(map[string]any)
 
 	// Call SetPath on an existing tree
 	err := SetPath(asMap, "nested.value", "changed")
@@ -70,13 +70,13 @@ func TestSetPathEmpty(t *testing.T) {
 	}
 
 	// Check that value was changed
-	if asMap["nested"].(map[string]interface{})["value"] != "changed" {
+	if asMap["nested"].(map[string]any)["value"] != "changed" {
 		t.Fatal("Expected .Nested.Value value is different")
 	}
 }
 
 func TestSetPathInvalid(t *testing.T) {
-	asMap := make(map[string]interface{})
+	asMap := make(map[string]any)
 	asMap["nested"] = 12
 
 	// Call SetPath on an existing tree
@@ -91,7 +91,7 @@ func TestSetPathInvalid(t *testing.T) {
 }
 
 func BenchmarkSetPath(b *testing.B) {
-	asMap := make(map[string]interface{})
+	asMap := make(map[string]any)
 	err := SetPath(asMap, "nested.value", "new-value")
 	if err != nil {
 		b.Fatal("Failed to set initial value")
